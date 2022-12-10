182a51fd29f63e4649a621711465b9e3d1d84168
無常 <3142324836@qq.com>
2022-12-11 01:38:14 +0800

    Update CMakeList

--- extras/demo1/demo1.cpp
+++ extras/demo1/demo1.cpp
@@ -1,31 +1,11 @@
 ﻿#include "../../LUCE.h"
 
-static int wrap_exceptions(lua_State* L, lua_CFunction f) {
-	try {
-		return f(L);
-	}
-	catch (const char* s) {
-		lua_pushstring(L, s);
-	}
-	catch (std::exception& e) {
-		lua_pushstring(L, e.what());
-	}
-	catch (...) {
-		lua_pushliteral(L, "caught (...)");
-	}
-	return lua_error(L);
-}
-
 int main(int argc, char* argv[]) {
 	lua_State* L = luaL_newstate();
 	if (!L) { return -1; }
 
 	int ret_value = 0;
 	try {
-		lua_pushlightuserdata(L, (void*)wrap_exceptions);
-		luaJIT_setmode(L, -1, LUAJIT_MODE_WRAPCFUNC | LUAJIT_MODE_ON);
-		lua_pop(L, 1);
-
 		luaL_openlibs(L);
 		
 		luce::createLUCETable(L);
